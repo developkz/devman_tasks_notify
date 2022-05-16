efdd817ca5efcdd541ffe1c716793cbac7ff23dc
[Alexandr] <[afominyh242@gmail.com]>
2022-05-16 17:42:56 +0700

    Added basic functionality

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from pprint import pprint
+
+import requests
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+def fetch_dvmn_long_polling():
+    headers = {
+        'Authorization': f'Token {dvmn_auth_token}'
+    }
+    params = {
+
+    }
+    dvmn_answer = requests.get(f'https://dvmn.org/api/long_polling/', headers=headers, params=params, timeout=5)
+    dvmn_answer.raise_for_status()
+    return dvmn_answer.json()
+
+
+if __name__ == '__main__':
+    env_path = Path('.') / '.env'
+    load_dotenv(env_path)
+    dvmn_auth_token = os.getenv('DVMN_TOKEN')
+
+    while True:
+        try:
+            pprint(fetch_dvmn_long_polling())
+        except requests.exceptions.ReadTimeout:
+            print('ReadTimeout')
+        except requests.exceptions.ConnectionError:
+            print('No Internet connection. Awaiting for internet connection...')
