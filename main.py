85cd1d7644aff61fbba47a8d505fb7ef2a859afc
[Alexandr] <[afominyh242@gmail.com]>
2022-05-17 19:37:55 +0700

    Get json of attempt check

--- main.py
+++ main.py
@@ -6,19 +6,14 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
+
 def fetch_dvmn_long_polling():
     headers = {
         'Authorization': f'Token {dvmn_auth_token}'
     }
-    params = {
-
-    }
-
     dvmn_answer = requests.get(f'https://dvmn.org/api/long_polling/', headers=headers, timeout=5)
     dvmn_answer.raise_for_status()
-    if dvmn_answer.ok:
-        time_stamp = dvmn_answer.json()['']
-        return time_stamp
+    return dvmn_answer.json()
     # return dvmn_answer.json()
 
 
